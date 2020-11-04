55a76b1cba5e65ce2549102ecb08de7174a74dfe
Unknowncall <zharvey3131@gmail.com>
2020-11-04 14:00:45 -0600

    Finsiehd Compute Sum and added comments to helper functions

--- PA3/MWIS.java
+++ PA3/MWIS.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MWIS extends Tree {
 
@@ -23,13 +24,15 @@ public class MWIS extends Tree {
 		if(computedSum[node] != Integer.MIN_VALUE) {
 			return computedSum[node];
 		}
-		int excl = 0;
-		int incl = weights[node];
-		ArrayList<Integer> children = new ArrayList<Integer>(adjList.get(node));
+		int excl = 0, incl = weights[node];
+		List<Integer> children = new ArrayList<>(adjList.get(node));
 		for(int i = 0; i < children.size(); i++) {
 			int child = children.get(i);
 			excl += computeSum(child);
-			//Not finished
+			List<Integer> grandChildren = new ArrayList<>(adjList.get(child));
+			for (int j = 0; j < grandChildren.size(); j++) {
+				incl += computeSum(grandChildren.get(j));
+			}
 		}
 		if(incl > excl) {
 			computedSum[node] = incl;
@@ -41,6 +44,7 @@ public class MWIS extends Tree {
 	}
 
 	public void computeSet(int root) { // complete this function
+		// Do we make excl and incl class variables????
 	}
 
 	private void computeSetHelper(int node, int parent) { // complete this function
