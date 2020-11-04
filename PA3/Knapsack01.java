31ab4e622630eca2947f829da26fb46ce6439fba
Unknowncall <zharvey3131@gmail.com>
2020-11-04 13:50:12 -0600

    Built out basics of Knapsack01

--- PA3/Knapsack01.java
+++ PA3/Knapsack01.java
@@ -2,5 +2,21 @@
 public class Knapsack01 {
 	
 	public static int findOptimalProfit(final int profits[], final int weights[], int numElements, int capacity) { // complete this function
+		int[] currentRow = new int[capacity + 1], preRow = new int[capacity + 1];
+		for (int i = 0; i < preRow.length; i++) {
+			preRow[i] = 0;
+		}
+		for (int i = 0; i < numElements; i++) {
+			if (weights[i] > capacity) {
+				// assign currentRow[j] = prevRow[j], where O <= j < weights[i]
+				for (int j = weights[i]; j <= capacity; j++) {
+					currentRow[j] = Math.max(preRow[j], preRow[j - weights[i]] + profits[i]);
+				}
+			}
+			for (int j = 0; i < currentRow.length; i++) {
+				currentRow[j] = preRow[j];
+			}
+		}
+		return currentRow[capacity];
 	}
 }
