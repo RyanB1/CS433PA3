7ffdec8b7e81bbdf671db792728751376c582e64
Unknowncall <zharvey3131@gmail.com>
2020-11-06 18:32:58 -0600

    Finished knapsack

--- PA3/Knapsack01.java
+++ PA3/Knapsack01.java
@@ -3,16 +3,13 @@ import java.util.Arrays;
 public class Knapsack01 {
 	
 	public static int findOptimalProfit(final int profits[], final int weights[], int numElements, int capacity) { // complete this function
-		int[] currentRow = new int[capacity + 1], preRow = new int[capacity + 1];
-		Arrays.fill(preRow, 0);
+		int[] currentRow = new int[capacity + 1], prevRow = new int[capacity + 1];
+		Arrays.fill(prevRow, 0);
 		for (int i = 0; i < numElements; i++) {
-			if (weights[i] > capacity) {
-				// assign currentRow[j] = prevRow[j], where O <= j < weights[i]
-				for (int j = weights[i]; j <= capacity; j++) {
-					currentRow[j] = Math.max(preRow[j], preRow[j - weights[i]] + profits[i]);
-				}
+			for (int j = capacity; j >= weights[i]; j--) {
+				currentRow[j] = Math.max(prevRow[j], prevRow[j - weights[i]] + profits[i]);
 			}
-			preRow = Arrays.copyOf(currentRow, currentRow.length);
+			prevRow = Arrays.copyOf(currentRow, currentRow.length);
 		}
 		return currentRow[capacity];
 	}
